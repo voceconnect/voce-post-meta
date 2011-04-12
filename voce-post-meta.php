041996b7f2f73b5854939cc42b901ba7eb1e699f
Michael Pretty <mpretty@voceconnect.com>
2011-04-12 13:27:46 -0400

    adding descriptions to group

--- voce-post-meta.php
+++ voce-post-meta.php
@@ -2,80 +2,118 @@
 
 class Voce_Meta_API {
 	private static $instance;
-
+	
 	var $groups;
-
+	
 	public static function GetInstance() {
-
-		if (!isset(self::$instance)) {
-			self::$instance = new Voce_Settings_API();
+		
+		if (! isset ( self::$instance )) {
+			self::$instance = new Voce_Meta_API ();
 		}
-
+		
 		return self::$instance;
 	}
-
+	
 	function add_group($id, $title, $args = array()) {
-
-		if (!isset($this->groups[$id])) {
-			$this->groups[$id] = new Voce_Meta_Group($id, $title, $args);
+		
+		if (! isset ( $this->groups [$id] )) {
+			$this->groups [$id] = new Voce_Meta_Group ( $id, $title, $args );
 		}
-
-		return $this->groups[$id];
+		
+		return $this->groups [$id];
 	}
 
 }
 
 class Voce_Meta_Group {
-	var $fields;
-	var $id;
-	var $title;
-	var $description;
-	var $capability;
-	var $context;
-	var $priority;
-
+	/**
+	 * 
+	 * Associative array of fields belonging to this group 
+	 * @var array
+	 */
+	private $fields;
+	
+	/**
+	 * 
+	 * ID of the group
+	 * @var string
+	 */
+	private $id;
+	
+	/**
+	 * 
+	 * Title of group
+	 * @var string
+	 */
+	private $title;
+	
+	/**
+	 * 
+	 * Descriptive text to display at the top of the metabox
+	 * @var string
+	 */
+	private $description;
+	
+	/**
+	 * 
+	 * Required capability to edit this group
+	 * @var string
+	 */
+	private $capability;
+	
+	/**
+	 * 
+	 * Context used for the metabox
+	 * @var string
+	 */
+	private $context;
+	
+	/**
+	 * 
+	 * priority for the metabox
+	 * @var string
+	 */
+	private $priority;
+	
 	function __construct($id, $title, $args) {
-		$defaults = array(
-			'description' => '',
-			'capability' => '',
-			'context' => 'normal',
-			'priority' => 'default',
-		);
-		$r = wp_parse_args($args, $defaults);
-
-		$this->fields = array();
+		$defaults = array ('description' => '', 'capability' => '', 'context' => 'normal', 'priority' => 'default' );
+		$r = wp_parse_args ( $args, $defaults );
+		
+		$this->fields = array ();
 		$this->id = $id;
 		$this->title = $title;
-		$this->description = $r['description'];
-		$this->capability = $r['capability'];
-		$this->context = $r['context'];
-		$this->priority = $r['priority'];
-
-		add_action('add_meta_boxes', array($this, 'add_metabox'));
+		$this->description = $r ['description'];
+		$this->capability = $r ['capability'];
+		$this->context = $r ['context'];
+		$this->priority = $r ['priority'];
+		
+		add_action ( 'add_meta_boxes', array ($this, 'add_metabox' ) );
 	}
-
+	
 	function add_metabox($post_type) {
-		if (post_type_supports($post_type, $this->id)) {
-			add_meta_box($this->id, $$this->title, array($this, 'display_group'), $post_type, $this->context, $this->priority);
+		if (post_type_supports ( $post_type, $this->id )) {
+			add_meta_box ( $this->id, $$this->title, array ($this, 'display_group' ), $post_type, $this->context, $this->priority );
 		}
 	}
-
+	
 	function display_group() {
 		if ($this->description) {
-			echo '<p>', esc_html($this->description), '</p>';
+			echo '<p>', esc_html ( $this->description ), '</p>';
 		}
-		foreach ($this->fields as $field) {
-			$field->display_field();
+		foreach ( $this->fields as $field ) {
+			$field->display_field ();
 		}
 	}
-
-	function add_field($id, $label, $args = array()) {
-
-		if (!isset($this->fields[$id])) {
-			$this->fields[$id] = new Voce_Meta_Field($id, $label, $args);
+	
+	function add_field($type, $id, $label, $args = array()) {
+		
+		if (! isset ( $this->fields [$id] )) {
+			if ($type instanceof Voce_Meta_Field) {
+				$this->fields [$id] = new $type ( $id, $label, $args );
+			}
 		}
-
-		return $this->fields[$id];
+		
+		return $this->fields [$id];
 	}
 }
 
